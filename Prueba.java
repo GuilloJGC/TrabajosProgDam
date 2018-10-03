50611f4b59200067888a8135d1b1d60cc50cdeaa
sadgams12 <37366174+sadgams12@users.noreply.github.com>
2018-10-03 12:34:55 +0200

    Y ahora también mi localidad de residencia

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba{
 	public static void main (String args[]){
 		System.out.println("Guillermo J. Gutiérrez Carbajo");
 		System.out.println("23 años");
+		System.out.println("León");
 	}
 }
