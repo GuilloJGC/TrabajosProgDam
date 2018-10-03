685f80f0aa5b8bf713b771fec1f67ce56e08f48d
sadgams12 <37366174+sadgams12@users.noreply.github.com>
2018-10-03 12:48:01 +0200

    Y por último aquí está mi fecha de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -3,5 +3,6 @@ public class Prueba{
 		System.out.println("Guillermo J. Gutiérrez Carbajo");
 		System.out.println("23 años");
 		System.out.println("León");
+		System.out.println("08/02/1995");
 	}
 }
